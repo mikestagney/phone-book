37f2deb4e15b25ca7f107b8efec21c5ce3499695
mikestagney <mikestagney@yahoo.com>
2021-10-21 19:47:22 -0400

    saving outline

--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@ package phonebook;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // open main file, store in list
+
+        // open search term file, store in list
+
+        // conduct linear search on main data
+        // record time to find it and print out
+
+
     }
 }
