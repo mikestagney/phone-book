00597d77879d494256b0922aff4658bfd6c86f0e
mikestagney <mikestagney@yahoo.com>
2021-10-29 16:06:51 -0400

    added bubble sort, jump search in progress

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 package phonebook;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -10,15 +9,15 @@ import java.util.List;
 // /Users/mikestagney/Downloads/small_find.txt
 
 public class Main {
-    public static final String dataFilePath = "/Users/mikestagney/Downloads/directory.txt";
-    public static final String searchFilePath = "/Users/mikestagney/Downloads/find.txt";
+    public static final String dataFilePath = "/Users/mikestagney/Downloads/small_directory.txt";
+    public static final String searchFilePath = "/Users/mikestagney/Downloads/small_find.txt";
 
     public static void main(String[] args) {
         List<String> dataSource = FileIO.readFromFile(dataFilePath);
         List<String> searchItems = FileIO.readFromFile(searchFilePath);
         int numberItemsToFind = searchItems.size();
 
-        System.out.println("Start searching...");
+        System.out.println("Start searching (linear search)...");
         Timer linearTimer = new Timer();
 
         int numItemsFound = LinearSearch.go(dataSource, searchItems);
@@ -27,6 +26,31 @@ public class Main {
         System.out.printf("Found %d / %d entries. Time taken: %d min. %d sec. %d ms.\n",
                 numItemsFound, numberItemsToFind, linearTimer.getMinutes(), linearTimer.getSeconds(), linearTimer.getMilliseconds() );
 
+        System.out.println("Start searching (bubble sort + jump search)...");
+        Timer jumpTimer = new Timer();
+        // add bubble sort
+        for (int i = 0; i < dataSource.size(); i++) {
+            for (int j = i + 1; j < dataSource.size(); j++) {
+                String[] firstTuple = dataSource.get(i).split(" ", 2);
+                String firstName = firstTuple[1];
+                String[] secondTuple = dataSource.get(j).split(" ",  2);
+                String secondName = secondTuple[1];
+                if (firstName.compareTo(secondName) > 0) {
+                    Collections.swap(dataSource, i, j);
+                }
+            }
+        }
+        dataSource.forEach(System.out::println);
+
+        // jump search
+        int jumpInterval = (int) Math.floor(Math.sqrt(dataSource.size()));
+        for (String name: searchItems) {
+            for (int i = 0; i < numberItemsToFind; i += jumpInterval) {
+
+
+
+            }
+        }
 
 
     }
